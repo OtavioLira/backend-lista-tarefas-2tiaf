67224a8393755280afb65c8d42bb3ed45ccbe5b9
OtavioLira <otavioliraneves@gmail.com>
2024-09-19 08:41:20 -0300

    feat - Criação do banco de dados

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json())
+
+// Cria um banco de dados SQlite em memória
+const sqlite3 = require("sqlite3").verbose()
+const db = new sqlite3.Database("lista-tarefas.db")
+
+// Cria a tabela 'tarefas' no banco de dados
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)")
+})
+
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
@@ -9,4 +20,6 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
+
 
